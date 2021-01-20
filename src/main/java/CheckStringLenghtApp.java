ef16663a542943282a75247ad7f9eb546a8c28f6
Daniel-Lohrey <daniel.lohrey@web.de>
2021-01-20 15:38:13 +0100

    First Maven Test

---
+++ src/main/java/CheckStringLenghtApp.java
@@ -0,0 +1,9 @@
+import java.sql.SQLOutput;
+
+public class CheckStringLenghtApp {
+
+    public static void main(String[] args) {
+        String textToCheck = "Hallo";
+        System.out.println(textToCheck);
+    }
+}
---
+++ src/test/java/CheckStringLenghtTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class CheckStringLenghtTest {
+    @Test
+    public void test(){
+
+
+    }
+}
